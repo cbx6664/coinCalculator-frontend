1b103cafbfd913685874f5a6332ab5897aabb23f
CBX <chenbingxu369@gmail.com>
2025-01-05 12:12:49 +0800

    feat/created-front-end-project

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     setError("");
     setResult([]);
     try {
-      const response = await fetch("http://localhost:8080/coins", {
+      const response = await fetch(`${API_BASE_URL}/coins`,  {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
